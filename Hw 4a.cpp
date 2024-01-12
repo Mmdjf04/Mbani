2770a5a17fa823342ce68d50278f973b1c717f3d
Mmdjf04 <108787538+Mmdjf04@users.noreply.github.com>
2024-01-12 21:17:25 +0330

    Add files via upload

---
+++ Hw 4a.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+float n1,n2,r;
+char op;
+cout<<"please enter expression"
+cin>>n1>>op>>n2;
+switch(op){
+case "+" :
+r=n1 + n2;
+break;
+case"-";
+r=n1 - n2;
+break;
+case "*"
+r= n1 * n2;
+break;
+case "/"
+r=n1 / n2;}
+ cout<<"result= "<<< r;
+}
---
+++ Hw5b.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+char character1;
+cout<<"please enter character:";
+cin>>character1;
+
+switch (character1){
+case 'a':
+cout<<"vowel";
+break;
+case 'A':
+cout<<"vowel";
+break;
+
+case 'e':
+	cout<<"vowel";
+	break;
+case'e':
+	cout<<"vowel";
+	break;
+case'I':
+cout<<"vowel";
+break;	
+case'i':
+	cout<<"vowel";
+	break;
+case'U':
+	cout<<"vowel";
+	break;
+	case 'u':
+		cout<<"vowel";
+		break;
+	case 'O':
+		cout<<"vowel";
+		break;
+	case 'o':
+	cout<<"vowel";
+	default : 
+	cout <<"not vowel";
+
+
+}}
